ab1dcef3a40a7e30d9421365be3ed86fd23f8955
anni-257 <taraleaniket257@gmail.com>
2022-08-17 15:52:41 +0530

    Sub Arrays In Array

---
+++ Arrays/SubArrays.java
@@ -0,0 +1,18 @@
+
+public class SubArrays{
+
+    public static void subArrays(int arr[]){
+        for(int i=0;i<arr.length;i++){
+                for(int j=i;j<arr.length;j++){
+                    System.out.print("(");
+                    for(int k=i;k<=j;k++){
+                        System.out.print(arr[k]+" ");
+                }System.out.print(") ");
+            }System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        subArrays(arr);
+    }
+}
