4b1ae760e6d17e1f328c5daafcabf73c336fd2bb
anni-257 <taraleaniket257@gmail.com>
2022-08-18 16:36:23 +0530

    BubbleSort

--- Basic_Sorting_Technique/BubbleSort.java
+++ Basic_Sorting_Technique/BubbleSort.java
@@ -1,7 +1,8 @@
 public class BubbleSort{
     public static void bubbleSort(int arr[]){
-        int flag=0;
+        
         for(int i=0;i<arr.length-1;i++){
+            int flag=0;
             for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
