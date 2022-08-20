e7c5e8f53154fd80275ff8fe227e90b2b9c36180
anni-257 <taraleaniket257@gmail.com>
2022-08-20 21:50:19 +0530

    Count Number of Elements Binary Search

---
+++ Arrays/CountNumberOfElements.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class CountNumberOfElements {
+    public static int countNumberOfElements(int ans[]){
+        if(ans[0]!=-1){
+            return ans[1]-ans[0]+1;
+        }
+
+        return 0;
+    }
+    public static int firstOccu(int arr[],int key){
+        int si=0,ei=arr.length-1;
+        int mid=si+(ei-si)/2;
+        int ans=-1;
+        while(si<=ei){
+            if(arr[mid]==key){
+                ans=mid;
+                ei=mid-1;
+            }else if(key<arr[mid]){
+                ei=mid-1;
+            }else{
+                si=mid+1;
+            }
+            mid=si+(ei-si)/2;
+        }
+        return ans;
+    }
+    public static int lastOccu(int arr[],int key){
+        int si=0,ei=arr.length-1;
+        int mid=si+(ei-si)/2;
+        int ans=-1;
+        while(si<=ei){
+            if(arr[mid]==key){
+                ans=mid;
+                si=mid+1;
+            }else if(key<arr[mid]){
+                ei=mid-1;
+            }else{
+                si=mid+1;
+            }
+            mid=si+(ei-si)/2;
+        }
+        return ans;
+    }
+    public static int [] firstAndLastOccurance(int arr[],int key){
+        int ans[]=new int[2];
+        int first=firstOccu(arr, key);
+        int last=lastOccu(arr, key);
+        ans[0]=first;
+        ans[1]=last;
+
+        return ans;
+    }
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        int arr[]={1,2,3,3,3,3,4,4,4,5,6,8,10,10,10,12,12};
+        System.out.print("Enter key: ");
+        int key=sc.nextInt();
+        int ans[]=firstAndLastOccurance(arr, key);
+        System.out.println("First Occurance: "+ans[0]);
+        System.out.println("Last Occurance: "+ans[1]);
+        System.out.println("Number Of Elements present: "+(countNumberOfElements(ans)));
+    }
+}
