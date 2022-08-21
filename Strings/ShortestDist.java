18e6e52323d41bf81d3526e9916af1e3e6dd6127
anni-257 <taraleaniket257@gmail.com>
2022-08-21 21:15:50 +0530

    Shortest Distance

---
+++ Strings/ShortestDist.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ShortestDist{
+    public static double shortestDist(String str){
+        int x=0,y=0;
+        for(int i=0;i<str.length();i++){
+            switch(str.charAt(i)){
+                case 'N':y++;break;
+                case 'S':y--;break;
+                case 'E':x++;break;
+                case 'W':x--;break;
+            }
+        }
+
+        return Math.sqrt(((x*x)+(y*y)));
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        double ans=shortestDist(str);
+        System.out.println("Shortest Distance: "+ans);
+    }
+}
