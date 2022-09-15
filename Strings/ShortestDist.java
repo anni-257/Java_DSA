8934dc26e629a0edd48532b36e1323fb65303e22
anni-257 <taraleaniket257@gmail.com>
2022-09-15 23:47:19 +0530

    ShortestDist

--- Strings/ShortestDist.java
+++ Strings/ShortestDist.java
@@ -4,10 +4,10 @@ public class ShortestDist{
         int x=0,y=0;
         for(int i=0;i<str.length();i++){
             switch(str.charAt(i)){
-                case 'N':y++;break;
-                case 'S':y--;  break;
-                case 'E':x++;  break;
-                case 'W':x--;break;
+                case 'N':y++ ;  break;
+                case 'S':y-- ;  break;
+                case 'E':x++ ;  break;
+                case 'W':x-- ;  break;
             }
         }
 
