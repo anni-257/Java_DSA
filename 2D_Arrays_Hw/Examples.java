4b819c2ed84675a768c4e96d0475cf1773cca8c1
anni-257 <taraleaniket257@gmail.com>
2022-10-16 22:59:49 +0530

    Added

--- 2D_Arrays_Hw/Examples.java
+++ 2D_Arrays_Hw/Examples.java
@@ -3,7 +3,7 @@ public class Examples {
 
     public static  int numOfTimes(int mat[][],int x){
         int rows= mat.length;
-        int cols= mat[0].length ;
+        int cols=  mat[0].length ;
         int cnt ;
         cnt=0;
         for(int i=0;i <rows;i++){
