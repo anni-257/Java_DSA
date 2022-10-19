7f49cac978bc556d6c62200b2682b8cbcfeb5dde
anni-257 <taraleaniket257@gmail.com>
2022-10-19 21:08:49 +0530

    EXAMPLE

--- 2D_Arrays_Hw/Examples.java
+++ 2D_Arrays_Hw/Examples.java
@@ -9,7 +9,7 @@ public class  Examples {
         for(int i=0;i <rows;i++){
             for(int j=0;j<cols;j++) {
                 if(mat[i]  [j]==x){
-                    cnt++ ;
+                    cnt++  ;
                 }
             }
         }
