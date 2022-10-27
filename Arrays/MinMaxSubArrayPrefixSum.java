9d53ef82e4083afca11216daca547c5ab164886f
anni-257 <taraleaniket257@gmail.com>
2022-10-27 10:22:57 +0530

    MinMaxSubArrayPrefixSum

--- Arrays/MinMaxSubArrayPrefixSum.java
+++ Arrays/MinMaxSubArrayPrefixSum.java
@@ -1,6 +1,8 @@
-public class MinMaxSubArrayPrefixSum{
-    public static int minSubArraySum(int arr[]){
-        int maxi=Integer.MIN_VALUE;
+
+class MinMaxSubArrayPrefixSum{
+
+    public static int maxSubArraySum(int []arr){
+        int maxSum=Integer.MIN_VALUE;
         int prefix[]=new int[arr.length];
         prefix[0]=arr[0];
         for(int i=1;i<arr.length;i++){
@@ -8,21 +10,40 @@ public class MinMaxSubArrayPrefixSum{
         }
 
         for(int i=0;i<arr.length;i++){
-            int currentSum=0;
             for(int j=i;j<arr.length;j++){
-                currentSum=i==0?prefix[j]:prefix[j]-prefix[i-1];
-                if(maxi<currentSum){
-                    maxi=currentSum;
-                } 
+                int currSum= i==0 ? prefix[j] : prefix[j]-prefix[i-1];
+                maxSum=Math.max(maxSum,currSum);
             }
-            
         }
 
-        return maxi;
+        return maxSum;
     }
-    public static void main(String arg[]){
-        int arr[]={1,-2,6,-1,3};
-        int ans=minSubArraySum(arr);
-        System.out.println("Maximum sum of subArray: "+ans);
+
+    public static int minSubArraySum(int []arr){
+
+        int minSum=Integer.MAX_VALUE;
+        int prefix[]=new int[arr.length];
+        prefix[0]=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                int currSum= i==0 ? prefix[j]: prefix[j]-prefix[i-1];
+                minSum=Math.min(minSum,currSum);
+            }
+        }
+
+        return minSum;
+
+    }
+    public static void main(String args[]){
+        int arr[]={1,-2,3,4,-6,-3,10};
+        int min=minSubArraySum(arr);
+        int max=maxSubArraySum(arr);
+        System.out.println("Minimum subArray value is: "+min);
+        System.out.println("Maximum subArray value is: "+max);
     }
 }
