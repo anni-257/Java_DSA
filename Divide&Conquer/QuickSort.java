a2fcaf5b11833988a66b0736886d8a332052c886
anni-257 <taraleaniket257@gmail.com>
2022-10-30 14:10:07 +0530

    QuickSort

---
+++ Divide&Conquer/QuickSort.java
@@ -0,0 +1,42 @@
+class QuickSort{
+
+    public static void print(int arr[]){
+        System.out.println("-----Array-----");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }System.out.println();
+    }
+
+    public static void quickSort(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int pivot=partition(arr,si,ei);
+        quickSort(arr, si, pivot-1);
+        quickSort(arr, pivot+1, ei);
+    }
+
+    public static int partition(int arr[],int si,int ei){
+        int pivot=arr[ei];
+        int i=si-1;
+
+        for(int j=si;j<=ei;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        return i;
+
+    }
+    public static void main(String args[]){
+        int arr[]={4,3,6,-1,200,-100,-1,3,400,1000,1000};
+        print(arr);
+        quickSort(arr, 0, arr.length-1);
+        print(arr);
+
+    }
+}
